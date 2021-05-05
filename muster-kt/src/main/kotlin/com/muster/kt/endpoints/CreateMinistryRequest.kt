fcf4b51772ae8ced53ed12ad5d2ef84046f829a2
Felipe Flores <felipe.flores@rea-group.com>
2021-05-05 21:00:14 +1000

    Add controller implementation for Ministry ep.
    Part of https://github.com/felipeeflores/poly-muster/issues/2

---
+++ muster-kt/src/main/kotlin/com/muster/kt/endpoints/CreateMinistryRequest.kt
@@ -0,0 +1,3 @@
+package com.muster.kt.endpoints
+
+data class CreateMinistryRequest(val name: String, val description: String)
---
+++ muster-kt/src/main/kotlin/com/muster/kt/model/Ministry.kt
@@ -0,0 +1,5 @@
+package com.muster.kt.model
+
+import java.util.*
+
+data class Ministry(val id: UUID, val name: String, val description: String)
