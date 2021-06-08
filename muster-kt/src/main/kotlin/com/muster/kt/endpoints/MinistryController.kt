877636e9df8b3c580059c8fb83816984a62decef
Felipe Flores <felipe.flores@rea-group.com>
2021-06-08 18:57:01 +1000

    Make controller return a Response.
    https://github.com/felipeeflores/poly-muster/issues/1

--- muster-kt/src/main/kotlin/com/muster/kt/endpoints/MinistryController.kt
+++ muster-kt/src/main/kotlin/com/muster/kt/endpoints/MinistryController.kt
@@ -1,24 +1,30 @@
 package com.muster.kt.endpoints
 
 import com.muster.kt.model.Ministry
+import org.http4k.core.Response
+import org.http4k.core.Status
 import java.util.*
 
 fun interface MinistryController {
-  fun handleSaveMinistry(ministry: CreateMinistryRequest): Optional<UUID>
+  fun handleSaveMinistry(ministry: CreateMinistryRequest): Response
 }
 
 class MinistryControllerImpl(
   private val getUUID: () -> UUID,
   private val saveMinistry: (Ministry) -> Optional<UUID>
 ) : MinistryController {
-  override fun handleSaveMinistry(createMinistryRequest: CreateMinistryRequest): Optional<UUID> {
-    return saveMinistry(
+  override fun handleSaveMinistry(createMinistryRequest: CreateMinistryRequest): Response {
+    val result = saveMinistry(
       Ministry(
         id = getUUID(),
         name = createMinistryRequest.name,
         description = createMinistryRequest.description
       )
     )
+
+    return result.map { uuid ->
+      Response(Status.OK).body(uuid.toString())
+    }.orElse(Response(Status.INTERNAL_SERVER_ERROR).body("unable to fulfil request"))
   }
 }
 
--- muster-kt/src/test/kotlin/com/muster/kt/endpoints/MinistryControllerImplTest.kt
+++ muster-kt/src/test/kotlin/com/muster/kt/endpoints/MinistryControllerImplTest.kt
@@ -2,8 +2,10 @@ package com.muster.kt.endpoints
 
 
 import com.muster.kt.model.Ministry
-import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.equalTo
+import com.natpryce.hamkrest.assertion.assertThat
+import org.http4k.core.Status
+import org.http4k.hamkrest.hasBody
+import org.http4k.hamkrest.hasStatus
 import org.junit.jupiter.api.Test
 import java.util.*
 
@@ -12,12 +14,26 @@ internal class MinistryControllerImplTest {
   private val testUUID = UUID.fromString("459a1eb6-ad8d-11eb-8529-0242ac130003")
   private val controller = MinistryControllerImpl(
     getUUID = { testUUID },
-    saveMinistry = { m: Ministry -> Optional.of(m.id)}
+    saveMinistry = { m: Ministry ->
+      if (m.name == "Foo") Optional.of(m.id) else Optional.empty()
+    }
   )
 
   @Test
   fun handleSaveMinistry_shouldSaveMinistry() {
     val cmr = CreateMinistryRequest("Foo", "Bar")
-    assertThat(controller.handleSaveMinistry(cmr), equalTo(Optional.of(testUUID)))
+    assertThat(controller.handleSaveMinistry(cmr), hasStatus(Status.OK))
+  }
+
+  @Test
+  fun handleSaveMinistry_shouldReturnBodyWithId() {
+    val cmr = CreateMinistryRequest("Foo", "Bar")
+    assertThat(controller.handleSaveMinistry(cmr), hasBody("459a1eb6-ad8d-11eb-8529-0242ac130003"))
+  }
+
+  @Test
+  fun handleSaveMinistry_shouldFailSavingWith500() {
+    val cmr = CreateMinistryRequest("Baz", "Blah")
+    assertThat(controller.handleSaveMinistry(cmr), hasStatus(Status.INTERNAL_SERVER_ERROR))
   }
 }
