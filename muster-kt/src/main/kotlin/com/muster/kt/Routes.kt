8bcbbad4794e02326504535dd0fef7b6a8c46344
Felipe Flores <felipe.flores@rea-group.com>
2021-06-08 18:58:11 +1000

    Add muster Routes.
    https://github.com/felipeeflores/poly-muster/issues/1

---
+++ muster-kt/src/main/kotlin/com/muster/kt/Routes.kt
@@ -0,0 +1,20 @@
+package com.muster.kt
+
+import com.muster.kt.endpoints.CreateMinistryRequest
+import org.http4k.core.Body
+import org.http4k.core.Method.POST
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.format.Jackson.auto
+import org.http4k.routing.bind
+import org.http4k.routing.routes
+
+class Routes(private val handleCreateMinistry: (CreateMinistryRequest) -> Response) {
+
+  private val createMinistryLens = Body.auto<CreateMinistryRequest>().toLens()
+
+  val musterApp = routes(
+    "/" bind POST to { request: Request -> handleCreateMinistry(createMinistryLens(request)) },
+  )
+
+}
